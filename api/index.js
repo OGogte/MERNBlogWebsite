4bdfc2daae53110d083acc5faeda616a4c314e52
OGogte <omg.omgogte@gmail.com>
2023-10-27 04:24:04 +0530

    Basic Login and register working with jwt and password hashing

--- api/index.js
+++ api/index.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
+const User = require('./models/User');
+
+
+mongoose.connect(process.env.MY_MONGO_URL)
 
 const app = express();
+const salt = bcrypt.genSaltSync(10);
+const secret = process.env.JWT_SECRET_KEY;
 
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:5173'}));
 app.use(express.json());
 
-app.post('/register', (req,res)=> {
-    const {username,password} = req.body;
-    res.json({requestData:{username,password}});
-})
 
+app.post('/register', async (req,res)=> {
+    const {username,password} = req.body;
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json({userDoc});
+    } catch(e) {
+        res.status(400).json(e);
+    }
+});
+
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password,userDoc.password);
+    if(passOk) {
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
+    }
+})
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,40 @@
-
-
+import { useState } from "react";
+import {Navigate} from "react-router-dom";
 export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  async function login(e) {
+    e.preventDefault();
+    const response = await fetch ('http://localhost:4000/login' , {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers : {'Content-Type' : 'application/json'},
+      credentials: 'include',
+    });
+    if(response.ok) {
+      setRedirect(true);
+    } else {
+      alert('wrong credentials')
+    }
+  }
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
   return (
-    <form action="" className="login">
-    <h1>Login</h1>
-    <input type="text" placeholder="Username" name="" id="" />
-    <input type="password" placeholder='Password' name="" id="" />
-    <button>Login</button>
+    <form className="login" onSubmit={login}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={e => setUsername(e.target.value)} />
+      <input
+        type="password"
+        placeholder='Password'
+        value={password}
+        onChange={e => setPassword(e.target.value)} />
+      <button>Login</button>
     </form>
   )
 }
