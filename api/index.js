279555c2adefeb90b97a863d86a5f7a61675bdcb
OGogte <omg.omgogte@gmail.com>
2023-10-27 17:09:34 +0530

    Logout working properly

--- api/index.js
+++ api/index.js
@@ -15,32 +15,35 @@ const salt = bcrypt.genSaltSync(10);
 const secret = process.env.JWT_SECRET_KEY;
 
 
-app.use(cors({credentials:true, origin:'http://localhost:5173'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/register', async (req,res)=> {
-    const {username,password} = req.body;
-    try{
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
         const userDoc = await User.create({
             username,
-            password:bcrypt.hashSync(password,salt),
+            password: bcrypt.hashSync(password, salt),
         });
-        res.json({userDoc});
-    } catch(e) {
+        res.json({ userDoc });
+    } catch (e) {
         res.status(400).json(e);
     }
 });
 
 
-app.post('/login', async (req,res) => {
-    const {username,password} = req.body;
-    const userDoc = await User.findOne({username});
-    const passOk = bcrypt.compareSync(password,userDoc.password);
-    if(passOk) {
-        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).json('ok');
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username,
+            });
         });
     } else {
         res.status(400).json('wrong credentials');
@@ -48,13 +51,17 @@ app.post('/login', async (req,res) => {
 })
 
 
-app.get('/profile' , (req,res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, (err,info) => {
-        if(err) throw err;
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
         res.json(info);
     });
 });
 
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
 
 app.listen(4000);
